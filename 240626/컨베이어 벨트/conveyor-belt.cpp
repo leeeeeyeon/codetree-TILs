d354baa548e7da10e6c9d5cb72c2c1bf70852378
Jeongyeon Lee <leeeee_yeon@naver.com>
2024-06-26 19:26:40 +0900

    Title: 컨베이어 벨트, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240626/컨베이어 벨트/conveyor-belt.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, t;
+    cin >> n >> t;
+    
+    vector<int> belt = {};
+    // 실제로는 2*n짜리 배열
+    for (int k = 0; k < 2; k++) {
+        vector<int> line = vector<int>(3, 0);
+        for(int i = 0; i < n; i++) {
+            cin >> line[i];
+        }
+        for (auto elem : line) belt.push_back(elem);
+    }
+
+    rotate(belt.begin(), belt.end()-t, belt.end());
+
+    for (int i = 0; i < n; i++) {
+        cout << belt[i] << ' ';
+    }
+    cout << '\n';
+    for (int i = n; i < 2*n; i++) {
+        cout << belt[i] << ' ';
+    }
+
+    return 0;
+}
