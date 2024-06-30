eb37e065d30b728e3fedeeeb3d9e8e799b6b3365
Jeongyeon Lee <leeeee_yeon@naver.com>
2024-06-30 22:47:35 +0900

    Title: 사각형 채우기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240630/사각형 채우기 3/rectangle-fill-3.cpp
+++ 240630/사각형 채우기 3/rectangle-fill-3.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <vector>
 
+#define MAX_N 1000
+#define MOD 1'000'00'007
 using namespace std;
-
-int mod = 1'000'00'007;
 
 int main() {
     ios::sync_with_stdio(false);
@@ -13,10 +13,16 @@ int main() {
     int n;
     cin >> n;
 
-    vector<int> dp = {1, 2, 7, 22};
+    vector<int> dp = vector<int>(MAX_N, 0);
 
-    for(int i=3; i<= n; i++) {
-        dp.push_back((dp[i-1]*2 + dp[i-2]*3 + dp[i-3]*2 + dp[i-4]*2) % mod);
+    dp[0] = 1;
+    dp[1] = 2;
+
+    for(int i = 2; i <= n; i++) {
+        dp[i] = (dp[i-1]*2 + dp[i-2]*3) % MOD;
+        for (int j = i-3; j >= 0; j--) {
+            dp[i] = (dp[i] + dp[j]*2) % MOD;
+        }
     }
 
     cout << dp[n];
