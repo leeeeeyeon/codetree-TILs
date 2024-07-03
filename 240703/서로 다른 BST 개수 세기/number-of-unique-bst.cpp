6e556e6f98f80a813ae15e5b30db116af4265905
Jeongyeon Lee <leeeee_yeon@naver.com>
2024-07-03 18:47:17 +0900

    Title: 서로 다른 BST 개수 세기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240703/서로 다른 BST 개수 세기/number-of-unique-bst.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    vector<int> dp = vector<int>(n+1, 0);
+    
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for(int i = 2; i <= n; i++) {
+        int k = 0;
+        for(int j = 0; j < n; j++) k += dp[j] * dp[n-j-1];
+
+        dp[i] = k;
+    }
+
+    cout << dp[n];
+
+    return 0;
+}
