97f55603f46b8b6c6e583d237c83c1e3641e05af
Jeongyeon Lee <leeeee_yeon@naver.com>
2024-07-25 22:50:47 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240725/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
+++ 240725/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
@@ -26,10 +26,10 @@ int main() {
         for (int j = 1; j <= n; j++) {
             if (i == 1) {
                 if (j == 1) dp[i][j] = board[i][j];
-                else dp[i][j] = min(board[i][j], board[i][j-1]);
+                else dp[i][j] = min(board[i][j], dp[i][j-1]);
             }
             else if (j == 1) {
-                dp[i][j] = min(board[i][j], board[i-1][j]);
+                dp[i][j] = min(board[i][j], dp[i-1][j]);
             }
             else {
                 dp[i][j] = min(board[i][j], max(dp[i-1][j], dp[i][j-1]));
