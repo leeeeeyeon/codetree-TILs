6347fbea6b53bc3a8c3c3da8025d793ef18301c7
Jeongyeon Lee <leeeee_yeon@naver.com>
2024-07-29 19:00:04 +0900

    Title: 가장 가까운 점, Time: 43ms, Memory: 1MB, Status: Passed - CodeTree

--- 240729/가장 가까운 점/nearest-point.cpp
+++ 240729/가장 가까운 점/nearest-point.cpp
@@ -15,7 +15,7 @@ struct cmp {
             if (get<1>(a) == get<1>(b)) {
                 return get<2>(a) > get<2>(b);
             }
-            return get<1>(a) > get<2>(b);
+            return get<1>(a) > get<1>(b);
         }
         return get<0>(a) > get<0>(b);
     }
