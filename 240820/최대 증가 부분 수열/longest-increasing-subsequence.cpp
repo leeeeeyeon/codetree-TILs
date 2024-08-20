df9fc93d328cc7823565f9a43ecac966ea0a71ef
Jeongyeon Lee <leeeee_yeon@naver.com>
2024-08-20 23:24:43 +0900

    Title: 최대 증가 부분 수열, Time: 6ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240820/최대 증가 부분 수열/longest-increasing-subsequence.cpp
+++ 240820/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -13,17 +13,17 @@ int main() {
     cin >> n;
 
     vector<int> arr = vector<int>(n, 0);
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         cin >> arr[i];
     }
 
     vector<int> dp;
     dp.push_back(0);
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         // dp 배열이 비어있거나 현재 원소가 마지막 원소보다 클 때
         if (dp.empty() || arr[i] > dp[dp.size()-1]) dp.push_back(arr[i]);
         else {
-            int lb = lower_bound(arr.begin(), arr.end(), arr[i]) - arr.begin();
+            int lb = lower_bound(dp.begin(), dp.end(), arr[i]) - dp.begin();
             dp[lb] = arr[i];
         }
     }
