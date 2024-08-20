77aac39b460f017979b471a253818a19f368f3e4
Jeongyeon Lee <leeeee_yeon@naver.com>
2024-08-20 23:19:12 +0900

    Title: 최대 증가 부분 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240820/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    vector<int> arr = vector<int>(n, 0);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    vector<int> dp;
+    dp.push_back(0);
+    for (int i = 0; i < n; i++) {
+        // dp 배열이 비어있거나 현재 원소가 마지막 원소보다 클 때
+        if (dp.empty() || arr[i] > dp[dp.size()-1]) dp.push_back(arr[i]);
+        else {
+            int lb = lower_bound(arr.begin(), arr.end(), arr[i]) - arr.begin();
+            dp[lb] = arr[i];
+        }
+    }
+
+    cout << dp.size() - 1;
+
+    return 0;
+}
