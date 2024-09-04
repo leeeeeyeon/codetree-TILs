d73b9f5e3cd4b59c94ace54e46a03142782bbfcf
leeeeeyeon <leeeee_yeon@naver.com>
2024-09-04 22:49:03 +0900

    Title: 집합의 원소, Time: 1054ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

--- 240904/집합의 원소/elements-of-a-set.cpp
+++ 240904/집합의 원소/elements-of-a-set.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int findParent(vector<int> parent, int x) {
     if (x == parent[x]) return parent[x];
 
-    return parent[x] = findParent(parent, x);
+    return parent[x] = findParent(parent, parent[x]);
 }
 
 void merge(vector<int> parent, int a, int b) {
@@ -39,14 +39,14 @@ int main() {
         if (cmd == 0) {
             // a가 포함된 집합과 b가 포함된 집합을 합침
             // 이미 같은 집합에 속한다면 아무 것도 하지 X
-            if (parent[a] == parent[b]) continue;
-            
+            if (findParent(parent, a) == findParent(parent, b)) continue;
+
             merge(parent, a, b);
         }
         else if (cmd == 1) {
             // a와 b가 같은 집합 > 1 출력
             // 그렇지 X > 0 출력
-            if (parent[a] == parent[b]) cout << 1 << '\n';
+            if (findParent(parent, a) == findParent(parent, b)) cout << 1 << '\n';
             else cout << 0 << '\n';
         }
     }
