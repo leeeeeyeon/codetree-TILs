0481e52ed135be9e4100ed589d7b9ddc4342746e
leeeeeyeon <leeeee_yeon@naver.com>
2024-10-17 23:13:33 +0900

    Title: 폭탄 해체 작업, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 241017/폭탄 해체 작업/the-bomb-dismantling.cpp
+++ 241017/폭탄 해체 작업/the-bomb-dismantling.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
-
-typedef pair<int, int> pii;
 
 #define SCORE first
 #define TIME second
 
-// 시간 제한이 작은 것 먼저
-// 같다면 점수가 큰 것 먼저
-bool comp(pii a, pii b) {
-    if (a.TIME == b.TIME) return a.SCORE > b.SCORE;
+bool comp(pair<int, int> a, pair<int, int> b) {
+    if (a.second == b.second) return a.SCORE > b.SCORE;
 
-    return a.TIME < b.TIME;
+    return a.second < b.second;
 }
 
 int main() {
@@ -22,32 +19,33 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n;
+    int n; // 폭탄의 수
     cin >> n;
-
-    vector<pii> bombs; // {점수, 시간 제한}
+    
+    vector<pair<int, int>> arr;
     int score, time;
     for (int i = 0; i < n; i++) {
         cin >> score >> time;
-        bombs.push_back({score, time});
+        arr.push_back({score, time});
     }
 
-    sort(bombs.begin(), bombs.end(), comp);
+    sort(arr.begin(), arr.end(), comp);
 
-    int t = 1; // 시간 흐름
-    int idx = 0;
-    int result = 0; // 얻을 수 있는 점수의 최댓값
-    while(t <= bombs[bombs.size()-1].TIME) {
-        // 시간 제한 내에 idx번째 폭탄 해체
-        if (bombs[idx].TIME >= t) {
-            result += bombs[idx].SCORE;
-            idx++;
-        } else { // bombs[idx].TIME < t
-            // 시간 제한이 끝나서 폭탄이 터짐
-            idx++;
+    int result = 0;
+    int maxT = arr[n-1].TIME;
+    int idx = n-1;
+
+    priority_queue<int> pq;
+    for (int t = maxT; t > 0; t--) {
+        while (idx >= 0 && arr[idx].TIME >= t) {
+            pq.push(arr[idx].SCORE);
+            idx--;
         }
 
-        t++;
+        if (!pq.empty()) {
+            result += pq.top();
+            pq.pop();
+        }
     }
 
     cout << result;
