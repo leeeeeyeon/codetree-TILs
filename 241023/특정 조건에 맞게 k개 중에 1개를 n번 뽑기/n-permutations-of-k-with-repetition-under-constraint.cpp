08f29973736942702cc3e38793685b3093ce5076
leeeeeyeon <leeeee_yeon@naver.com>
2024-10-23 22:53:45 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241023/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printNumbers(int k, int n, vector<int> &arr) {
+    if (arr.size() == n) {
+        int cnt = 0;
+        int target = arr[0];
+        for (int elem : arr) {
+            if (elem == target) cnt++;
+            else target = elem;
+        }
+
+        if (cnt < 3) {
+            for (int elem : arr) cout << elem << ' ';
+            cout << '\n';
+        }
+        
+        return;
+    }
+
+    for (int i = 1; i <= k; i++) {
+        arr.push_back(i);
+        printNumbers(k, n, arr);
+        arr.pop_back();
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int k, n;
+    cin >> k >> n;
+
+    vector<int> arr;
+    printNumbers(k, n, arr);
+
+    return 0;
+}
